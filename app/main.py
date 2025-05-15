5a06c2cdb81be99fe09f3dc5232f82d051533f33
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2025-05-15 16:03:48 -0300

    feito aleteações na main

--- app/main.py
+++ app/main.py
@@ -5,15 +5,20 @@ from app.endpoints.api_endpoint import router as api_router
 
 app = FastAPI()
 
-# Configure CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with the frontend's URL
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Register the endpoints
 app.include_router(user_router, prefix="/api", tags=["cadastro"])
+
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir apenas o front-end
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
+
+# Registra os endpoints
+app.include_router(user_router, prefix="/api", tags=["users"])
 app.include_router(api_router, prefix="/api", tags=["games"])
