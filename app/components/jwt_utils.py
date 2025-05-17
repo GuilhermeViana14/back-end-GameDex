cbb95ec8908f9308b7385eb96a2f848646a80767
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2025-05-17 18:45:38 -0300

    organizado o codigo e arrumado as classes

--- app/components/jwt_utils.py
+++ app/components/jwt_utils.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
 
-SECRET_KEY = "vevbr83bv83rbv38ybv3by8v3rb8vr3"  # Troque por uma chave forte e segura!
+SECRET_KEY = "vevbr83bv83rbv38ybv3by8v3rb8vr3"  
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/login")
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -14,3 +17,14 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
 def decode_access_token(token: str):
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = decode_access_token(token)
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return email
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
---
+++ app/schemas/game.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class GameCreate(BaseModel):
+    name: str
+    rawg_id: int
+    background_img: Optional[str] = None
+    platforms: Optional[str] = None
+    comment: Optional[str] = None
+    rating: Optional[int] = None
+    progress: Optional[str] = None
